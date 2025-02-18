b66c8787e9a40608c8a777ab7e9d17c78c55e2e3
Gn0rl <gnrltv4@gmail.com>
2025-02-18 11:49:04 +0400

    Prettier config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to GameHosting Reboot!</h1>
-      <p>This is a re-booted version of the GameHosting website.</p>
-      <p>You can find the original codebase at <a href="https://github.com/gn0rl/gamehosting">https://github.com/gn0rl/gamehosting</a></p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Welcome to GameHosting Reboot!</h1>
+			<p>This is a re-booted version of the GameHosting website.</p>
+			<p>
+				You can find the original codebase at{' '}
+				<a href="https://github.com/gn0rl/gamehosting">
+					https://github.com/gn0rl/gamehosting
+				</a>
+			</p>
+		</div>
+	)
 }
