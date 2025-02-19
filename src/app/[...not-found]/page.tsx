809d19380ee1662a5402bf0ea6be57cc3cf6465d
Gn0rl <gnrltv4@gmail.com>
2025-02-19 19:27:47 +0400

    Header

--- src/app/[...not-found]/page.tsx
+++ src/app/[...not-found]/page.tsx
@@ -1,3 +1,3 @@
 export default function page() {
-    return <div>Not Found</div>;
+	return <div>Not Found</div>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
+import { Header } from '@/layers/widgets/header'
+
 export default function Home() {
 	return (
 		<div>
+			<Header />
 			<h1>Welcome to GameHosting Reboot!</h1>
 			<p>This is a re-booted version of the GameHosting website.</p>
-			<p>
-				You can find the original codebase at{' '}
-				<a href="https://github.com/gn0rl/gamehosting">
-					https://github.com/gn0rl/gamehosting
-				</a>
-			</p>
+			<p>You can find the original codebase at </p>
 		</div>
 	)
 }
