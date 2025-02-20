f517443c7258d7e940a2b22d7d95a54df96fcf12
Gn0rl <gnrltv4@gmail.com>
2025-02-20 19:17:47 +0400

    Header Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
 	return (
 		<div>
 			<Header />
-			<h1>Welcome to GameHosting Reboot!</h1>
-			<p>This is a re-booted version of the GameHosting website.</p>
-			<p>You can find the original codebase at </p>
+			<h1>GAMEHOSTING REBOOT</h1>
+			<p>Это обновлённая версия GameHosting</p>
+			<p></p>
 		</div>
 	)
 }
